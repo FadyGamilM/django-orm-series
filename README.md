a7d3dc8b0b9319fb883761bf37161f24367aa9ce
FadyGamilM <gamilfady605@gmail.com>
2025-04-21 00:37:32 +0200

    data models + migrations + superuser creation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # django-orm-series
 learning django orm from this course : `https://www.youtube.com/playlist?list=PL-2EBeDYMIbQXKsyNweppuFptuogJe2L-`
+
+# Modeling Data From Django To Actual DB:
+1. Naming the FK fields as `[resource]` will reflect on the database as `[resource]_id`.
+2. Not Specifying the `id` field and let django define it will mark it as `serialize=False` and won't be serialzied to json.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# TODO : [The id field is marked as serialize=False so we need to take care of this later]
+class Resturant(models.Model):
+    class CusineType(models.TextChoices):
+        INDIAN = 'IN', 'Indian'
+        ITALIAN = 'IT', 'Italian'
+        CHINESE = 'CH', 'Chinese'
+        MEXICAN = 'MX', 'Mexican'
+        AMERICAN = 'AM', 'American'
+
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    opened_at = models.DateField()
+    website = models.URLField(default="")
+    causine = models.CharField(
+        max_length=2, choices=CusineType.choices, default=CusineType.AMERICAN)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    stars = models.PositiveSmallIntegerField()
+    comment = models.TextField()
+    resturant = models.ForeignKey(
+        Resturant, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='ratings')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.resturant.name} - {self.stars} stars"
+
+
+class Sale(models.Model):
+    resturant = models.ForeignKey(
+        Resturant, on_delete=models.CASCADE, related_name='sales')
+    saled_at = models.DateTimeField()
+    income = models.DecimalField(max_digits=10, decimal_places=2)
