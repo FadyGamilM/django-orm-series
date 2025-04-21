41101b15ef9b9a03045003e66b6a6b6eaaaa9e29
FadyGamilM <gamilfady605@gmail.com>
2025-04-21 04:48:43 +0200

    learning multiple ways of querying databases with foreign key relations and reverse relations and how ot optimize some select queries

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # django-orm-series
 learning django orm from this course : `https://www.youtube.com/playlist?list=PL-2EBeDYMIbQXKsyNweppuFptuogJe2L-`
 
-# Modeling Data From Django To Actual DB:
+# Notes On Modeling Data From Django To Actual DB:
 1. Naming the FK fields as `[resource]` will reflect on the database as `[resource]_id`.
 2. Not Specifying the `id` field and let django define it will mark it as `serialize=False` and won't be serialzied to json.
+
+
+# RunScript utility in Django: 
+- we can use a utility called `runscript` to run direct scripts inside our app 
+```shell
+└─▪ cd [your_app]
+└─▪ mkdir scripts
+└─▪ touch __init__.py
+└─▪ touch [script_name].py
+└─▪ python3 manage.py 
+```
+- now define a `run()` function and to run the script
+```shell
+└─▪ python3 manage.py runscript [script_name] # without .py
+```
+
+# Notes about Queries in Django
+1. Django queries are lazely executed:
+
+```python
+def select_first_resurant() -> Resturant:
+    first_resurant = Resturant.objects.first() # so far the SELECT statment query is not executed yet 
+    return first_resurant # once we use the fetched data .. the query will be executed 
+```
+
+
+2. Queryset .vs. Single object ? 
+```python
+def get_resturant_name_by_rating_id(rating_id):
+    rating = Rating.objects.filter(id=rating_id)
+    resturant = rating.resturant  # NOT VALID and NOT COMPILED
+    rating = Rating.objects.first()
+    resturant = rating.resturant  # VALID and COMPILED
+    return resturant.name
+```
+![](queryset_vs_single_object.png)
+
+- solution 
+```python
+def get_resturant_name_by_rating_id(rating_id):
+    rating = Rating.objects.get(id=rating_id)
+    resturant = rating.resturant  # NOT VALID and NOT COMPILED
+    return resturant.name
+```
---
+++ core/scripts/orm_script.py
@@ -0,0 +1,109 @@
+from core.models import Resturant, Rating, Sale
+from django.utils import timezone
+from django.db import connection
+from django.contrib.auth.models import User
+from django.db import transaction
+from pprint import pprint
+
+
+def run():
+    print('------------------ orm_script started execution ------------------')
+    # create_resturant_record_1()
+
+    # resurant = select_first_resurant()
+    # print(f'first resurant is {resurant} ')
+
+    # rate_resturant_1()
+
+    # rate_resturant_1_optimized()
+
+    # filter_rates_by_stars(4)
+
+    # get_resturant_name_by_rating_id(1)
+
+    # get_ratings_given_resturant()
+
+    get_user_rating_on_specific_resturant_or_create_new_one(1, 2)
+
+    for query in connection.queries:
+        print()
+        print(query)
+        print()
+    print('------------------ orm_script finished execution ------------------')
+
+
+def create_resturant_record_1():
+    resurant = Resturant()
+    resurant.name = 'Italizano'
+    resurant.latitude = 50.3
+    resurant.longitude = 50.301
+    resurant.causine = resurant.CusineType.ITALIAN
+    resurant.opened_at = timezone.now()
+
+    resurant.save()
+
+
+def select_first_resurant() -> Resturant:
+    # so far the SELECT statment query is not executed yet
+    first_resurant = Resturant.objects.first()
+    return first_resurant  # once we use the fetched data .. the query will be executed
+
+
+def rate_resturant_1():
+    with transaction.atomic():
+        resturant = Resturant.objects.first()
+        user = User.objects.first()
+        rating = Rating()
+        rating.stars = 4
+        rating.comment = 'Great food'
+        rating.resturant = resturant
+        rating.user = user
+
+        rating.save()
+
+
+def rate_resturant_1_optimized():
+    with transaction.atomic():
+        resturant_id = Resturant.objects.only('id').all()[1]
+        user_id = User.objects.only('id').first()
+        rating = Rating()
+        rating.stars = 4
+        rating.comment = 'Great food'
+        rating.resturant = resturant_id
+        rating.user = user_id
+
+        rating.save()
+
+
+def filter_rates_by_stars(stars):
+    with transaction.atomic():
+        ratings = Rating.objects.filter(stars__gt=stars)
+        print(f'found {len(ratings)} ratings with stars {stars}')
+
+
+def get_resturant_name_by_rating_id(rating_id):
+    rating = Rating.objects.get(id=rating_id)
+    resturant = rating.resturant  # NOT VALID and NOT COMPILED
+    print(f'resturant name is {resturant.name} ')
+
+
+def get_ratings_given_resturant():
+    resturant = Resturant.objects.first()
+    ratings = resturant.ratings.all()
+    print(ratings)
+
+
+def get_user_rating_on_specific_resturant_or_create_new_one(user_id, resturant_id):
+    with transaction.atomic():
+        resturant = Resturant.objects.get(id=resturant_id)
+        user = User.objects.get(id=user_id)
+        rating, is_created = Rating.objects.get_or_create(
+            resturant=resturant,
+            user=user,
+            stars=3
+        )
+        if is_created:
+            print('rating created')
+        else:
+            print('rating already exists')
+    print(rating)
